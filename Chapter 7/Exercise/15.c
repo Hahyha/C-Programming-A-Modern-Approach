69eebbf6f570544aeab723adfb0c01feded251f7
Hahyha <155079870+Hahyha@users.noreply.github.com>
2024-02-22 17:14:14 +0800

    Update 15.c

--- Chapter 7/Exercise/15.c
+++ Chapter 7/Exercise/15.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-  typedef char Int8
-  typedef short Int16
-  typedef int Int32
+  typedef char Int8;
+  typedef short Int16;
+  typedef int Int32;
 
   printf("%zu\n", sizeof(Int8));
   printf("%zu\n", sizeof(Int16));
