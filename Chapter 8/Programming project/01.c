379523a7a1245c0670d2e855df7ab57f4adfedaa
Hahyha <155079870+Hahyha@users.noreply.github.com>
2024-02-24 16:19:11 +0800

    Update 01.c

--- Chapter 8/Programming project/01.c
+++ Chapter 8/Programming project/01.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(void){
+    int i, digit_seen[10] = {0};
+    long number;
+    
+    printf("Enter a number: ");
+    scanf("%ld", &number);
+    
+    while (number){
+        digit_seen[number % 10] += 1;
+        number /= 10;
+    }
+
+    printf("Repeated digit(s):");
+    for (i = 0; i < 10; i++){
+        if (digit_seen[i] > 1) printf("%2d", i);
+    }
+    
+    return 0;
+}
