11d41ffcdf94df07d0547a732a88511c8b4e8240
Hahyha <155079870+Hahyha@users.noreply.github.com>
2024-02-29 23:24:25 +0800

    Update 02.c

--- Chapter 9/Exercise/02.c
+++ Chapter 9/Exercise/02.c
@@ -1,4 +1,3 @@
 int check(int x, int y, int n) {
-    if ((x > 0) && (x < (n - 1)) && (y > 0) && (y < (n - 1))) return 1;
-    else return 0;
+    return ((x > 0) && (x < (n - 1)) && (y > 0) && (y < (n - 1));
 }
